48dc6a938db35d24018d1db20a4192071b025456
lotmek <meklocdn@gmail.com>
2024-09-22 18:45:39 -0400

    🎉 Initialize plugin

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import { RuleModule } from "@typescript-eslint/utils/ts-eslint";
+import { ESLint } from "eslint";
+import { rules } from "./rules";
+
+const packageInfo = require("../package.json");
+
+type RuleKey = keyof typeof rules;
+
+interface Plugin extends Omit<ESLint.Plugin, "rules"> {
+  rules: Record<RuleKey, RuleModule<any, any, any>>;
+}
+
+const plugin: Plugin = {
+  meta: {
+    name: packageInfo.name,
+    version: packageInfo.version,
+  },
+  rules,
+};
+
+export default plugin;
