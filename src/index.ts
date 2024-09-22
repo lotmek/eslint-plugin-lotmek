e42f299b43dba82afc5ca58cee24d5b9e7123b8a
lotmek <meklocdn@gmail.com>
2024-09-22 18:52:43 -0400

    🧑‍💻 Add linter

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,13 @@ import { RuleModule } from "@typescript-eslint/utils/ts-eslint";
 import { ESLint } from "eslint";
 import { rules } from "./rules";
 
+// eslint-disable-next-line @typescript-eslint/no-require-imports
 const packageInfo = require("../package.json");
 
 type RuleKey = keyof typeof rules;
 
 interface Plugin extends Omit<ESLint.Plugin, "rules"> {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   rules: Record<RuleKey, RuleModule<any, any, any>>;
 }
 
